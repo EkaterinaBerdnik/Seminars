33b46dd3ce4cafe626f6d400901521d2fb93fa49
Ekaterina Berdnik <ek.berdnik@yandex.ru>
2022-12-23 14:21:26 +0300

    Homework4

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,72 +1,72 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿// // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
-
-
-int SecondNumber(int num)
-{
-    return (num % 100) / 10;
-}
-int randomNumber = new Random().Next(100, 1000);
-Console.WriteLine(randomNumber);
-int result = SecondNumber(randomNumber);
-Console.WriteLine(result);
+// // 456 -> 5
+// // 782 -> 8
+// // 918 -> 1
 
 
-
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-// 645 -> 5
-
-// 78 -> третьей цифры нет
-
-// 32679 -> 6
-
-
-Console.WriteLine("Введите число ");
-int num = Convert.ToInt32(Console.ReadLine());
-void ThirdNumber(int num)
-{
-    if (num < 100)
-    {
-        Console.WriteLine($"{num} -> Третьей цифры нет");
-    }
-    while (num >= 1000) num = num / 10;
-    {
-        if (num >= 100 && num < 1000) num = num % 10;
-        {
-            Console.WriteLine($"Третья цифра {num} ");
-        }
-    }
-}
-
-ThirdNumber(num);
+// int SecondNumber(int num)
+// {
+//     return (num % 100) / 10;
+// }
+// int randomNumber = new Random().Next(100, 1000);
+// Console.WriteLine(randomNumber);
+// int result = SecondNumber(randomNumber);
+// Console.WriteLine(result);
 
 
 
+// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// // 645 -> 5
+
+// // 78 -> третьей цифры нет
+
+// // 32679 -> 6
 
 
-// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Console.WriteLine("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// void ThirdNumber(int num)
+// {
+//     if (num < 100)
+//     {
+//         Console.WriteLine($"{num} -> Третьей цифры нет");
+//     }
+//     while (num >= 1000) num = num / 10;
+//     {
+//         if (num >= 100 && num < 1000) num = num % 10;
+//         {
+//             Console.WriteLine($"Третья цифра {num} ");
+//         }
+//     }
+// }
 
-// 6 -> да
-// 7 -> да
-// 1 -> нет
+// ThirdNumber(num);
 
-Console.WriteLine("Введите номер дня недели ");
-int num = int.Parse(Console.ReadLine()!);
 
-if (1 > num || num > 7)
-{
-    Console.WriteLine("Это не номер дня недели!");
-}
 
-else if (num == 6 || num == 7)
-{
-    Console.WriteLine("Это выходной день!");
-}
-else
-{
-    Console.WriteLine("Это не выходной день!");
-}
+
+
+// // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// // 6 -> да
+// // 7 -> да
+// // 1 -> нет
+
+// Console.WriteLine("Введите номер дня недели ");
+// int num = int.Parse(Console.ReadLine()!);
+
+// if (1 > num || num > 7)
+// {
+//     Console.WriteLine("Это не номер дня недели!");
+// }
+
+// else if (num == 6 || num == 7)
+// {
+//     Console.WriteLine("Это выходной день!");
+// }
+// else
+// {
+//     Console.WriteLine("Это не выходной день!");
+// }
---
+++ Homework4/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void step (int a, int b)
+{
+    Console.WriteLine($"{a}^{b} = {Math.Pow(a, b)}");
+}
+
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+step(a, b);
+
+
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (number > 0)
+{
+    result += number % 10;
+    number /= 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе = {result}");
+
+
+
+
+
+// // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// // 6, 1, 33 -> [6, 1, 33]
+
+
+
+int[] NewArray(int size)
+{
+    int[] myArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        myArray[i] = new Random().Next(size + 1);
+        Console.Write(myArray[i] + " ");
+    }
+    return myArray;
+}
+
+Console.WriteLine("Введите размер массива:");
+int lenght = Convert.ToInt32(Console.ReadLine());
+
+int[] Array = NewArray(lenght);
