e3314b5500458eac5e2d15faadd5356e10569245
Ekaterina Berdnik <ek.berdnik@yandex.ru>
2022-12-29 18:58:48 +0300

    Homework7

---
+++ Homework7/Program.cs
@@ -0,0 +1,159 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = (Convert.ToDouble(new Random().Next(0, 100)) / 10);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[rows, columns];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+
+
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] FillArrayRandomNumbers(int rows, int columns)
+{
+    int[,] newArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newArray[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FindElement (int[,] array, int row, int column)
+{
+    if(row < array.GetLength(0) || column < array.GetLength(1))  
+    Console.WriteLine($"Значение элемента массива {row} строки и {column} столбца равно {array[row-1,column-1]}"); 
+    else                                       
+    Console.WriteLine("Такого элемента в массиве нет");
+    return array;
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер строки");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int [,] array = FillArrayRandomNumbers(rows, columns);
+
+PrintArray(array);
+FindElement(array, row, column);
+
+
+
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] FillArrayRandomNumbers(int rows, int columns)
+{
+    int[,] newArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newArray[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FindSA(int[,] array)
+{
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+        sum = sum + array[i,j];
+        }
+        Console.WriteLine($"Среднее арифметическое столбца {j+1}: {Math.Round(sum/array.GetLength(0),2)}");
+        sum = 0;
+    }   
+    return array;
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите номер строки");
+// int i = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите номер столбца");
+// int j = Convert.ToInt32(Console.ReadLine());
+
+int [,] array = FillArrayRandomNumbers(rows, columns);
+PrintArray(array);
+FindSA(array);
